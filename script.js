4e892304bd032a93045a45980a5ba4e155137487
Monika Janiak <monika.janiak@windowslive.com>
2019-12-19 20:01:02 +0100

    edit files

--- script.js
+++ script.js
@@ -31,3 +31,29 @@ var flkty = new Flickity( '.main-carousel', {
 	}	
 	 
 })();  
+
+(function(){ 
+	
+	
+  	window.initMap = function() {
+		
+		
+		var uluru = {lat: -25.363, lng: 131.044};
+		
+		
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 4,
+			center: uluru
+		});
+		
+		
+		var marker = new google.maps.Marker({
+			position: uluru,
+			map: map
+		}); 
+	}	
+	 
+})();  
+
+
+initMap();
