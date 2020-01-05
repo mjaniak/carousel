5ea71b250761dee0417be500d052f59e7991fc96
kamil stobiecki <kamil.stobiecki@neoteric.eu>
2020-01-05 11:04:00 +0100

    project

--- script.js
+++ script.js
@@ -1,59 +1,65 @@
 'use strict';
 
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
+// var elem = document.querySelector('.carousel');
+// var flkty = new Flickity( elem, {
+//   // options
+//   cellAlign: 'left',
+//   contain: true
+// });
+
+// external js: flickity.pkgd.js
+
+var flkty = new Flickity('.carousel');
+
+var progressBar = document.querySelector('.progress-bar');
+
+flkty.on( 'scroll', function( progress ) {
+	progress = Math.max( 0, Math.min( 1, progress ) );
+	progressBar.style.width = progress * 100 + '%';
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
 
-(function(){ 
-	
+(function(){
+
   	window.initMap = function() {
-		
+
 		var uluru = {lat: -25.363, lng: 131.044};
-		
+
 		var map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 4,
 			center: uluru
 		});
-		
-		var marker = new google.maps.Marker({ 
-			position: uluru,
-			map: map
-		}); 
-	}	
-	 
-})();  
 
-(function(){ 
-	
-	
-  	window.initMap = function() {
-		
-		
-		var uluru = {lat: -25.363, lng: 131.044};
-		
-		
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 4,
-			center: uluru
-		});
-		
-		
 		var marker = new google.maps.Marker({
 			position: uluru,
 			map: map
-		}); 
-	}	
-	 
-})();  
+		});
+	}
+
+})();
+
+(function(){
+
+
+  	window.initMap = function() {
+
+
+		var uluru = {lat: -25.363, lng: 131.044};
+
+
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 4,
+			center: uluru
+		});
+
+
+		var marker = new google.maps.Marker({
+			position: uluru,
+			map: map
+		});
+	}
+
+})();
 
 
 initMap();
